54fce0621a26f6e28a997f56437e09c66de0beeb
jamilxt <jamilxt@gmail.com>
2025-04-27 21:31:36 +0600

    initial commit

---
+++ src/main/java/com/pondit/portfolio/controller/RootController.java
@@ -0,0 +1,29 @@
+package com.pondit.portfolio.controller;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class RootController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html; charset=UTF-8");
+        response.getWriter().print("""
+                <!doctype html>
+                <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport"
+                          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+                    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                    <title>Portfolio</title>
+                </head>
+                <body>
+                    <h1>Welcome to My Portfolio</h1>
+                </body>
+                </html>
+                """);
+    }
+}
